0f588b632035cbb879e13018a83946b17832a7a9
NataliaAiz <natasharelashio@gmail.com>
2017-11-26 02:07:05 +0700

    Структура html кода

--- www/index.php
+++ www/index.php
@@ -220,57 +220,68 @@ if($_SESSION["bool"]===1)
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 <body>
-     <header>
-	 <form name="Select1" action="" method="post"> <button type="submit" name="select3" value="Выбрать2"><img  src="<?="tri.png";?>"width="40" height="27";/></button> </form> 
-	  
-	  
-	  
-	  
-	  <form name="exit" action="" method="post">
+    <header>
+
+	 	<form name="Select1" action="" method="post"> 
+	 		<button type="submit" name="select3" value="Выбрать2">
+		 		<img  src="<?="tri.png";?>"width="40" height="27";/>
+		 	</button> 
+		</form> 
+	    	    
+	  	<form name="exit" action="" method="post">
 			<button type="submit" name="exit" value="Выход">Выход</button>
-	  </form>
+	 	 </form>
 	
+		<div id="logtext"> <img id="logo" src="logotext.png" /> </div>
+
+	  	<div id="vk"> 
+		  <a href="https://oauth.vk.com/authorize?client_id=<?=ID?>&display=page&redirect_uri=<?=URL?>&response_type=code" target="_blank">
+		  <img src="vk.png" height="50" width="60"/></a>
+		</div>
+
+	</header>
 	
-	  <div id="logtext"> <img id="logo" src="logotext.png" /> </div>
-	  <div id="vk"> <a href="https://oauth.vk.com/authorize?client_id=<?=ID?>&display=page&redirect_uri=<?=URL?>&response_type=code" target="_blank"><img src="vk.png" height="50" width="60"/>  </a></div>
-	 </header>
-	
-	 <div>
-	<form name="Select" action="" method="post">
+	<div>
+		<form name="Select" action="" method="post">
 		
-		 <div id="block1">   
-			 <button id="photo1" type="submit" name="select1" value="<?=$user1["Id"];?>"> <img  src="<?=$user1["Photo"];?>" width="265" height="265"; alt="<?=$user1["Id"];?>"/> </button> 
-			 <p id="info1"><? echo $user1[firstname]; ?></p>
-			<button class="btn btn-info btn-lg btn-block" name="message1" type="submit" value="<?=$user1["Id"];?>" <?=$key_mess1;?>> Сообщение <?echo $price1?>$</button>
+		 	<div id="block1">   
+			 	<button id="photo1" type="submit" name="select1" value="<?=$user1["Id"];?>"> 
+			 		<img  src="<?=$user1["Photo"];?>" width="265" height="265"; alt="<?=$user1["Id"];?>"/> 
+			 	</button> 
 			
-			<button class="btn btn-info btn-lg btn-block" name="f1" type="submit" value="<?=$user1["Id"];?>" > добавить в избранное </button>
-		 </div> 
+				<p id="info1"><? echo $user1[firstname]; ?></p>
+			
+				<button class="btn btn-info btn-lg btn-block" name="message1" type="submit" value="<?=$user1["Id"];?>" <?=$key_mess1;?>> Сообщение <?echo $price1?>$</button>
+			
+				<button class="btn btn-info btn-lg btn-block" name="f1" type="submit" value="<?=$user1["Id"];?>" > добавить в избранное </button>
+		 	
+			</div> 
 		 
-		  <div id="or"> <img src="or.png" height="50" width="60" /> </div>
+			<div id="or"> <img src="or.png" height="50" width="60" /> </div>
 		  
-		 <div id="block2"> 
-			 <button id="photo2" type="submit" name="select2" value="<?=$user2["Id"];?>"><img  src="<?=$user2["Photo"];?>"width="265" height="265"; alt="<?=$user2["Id"];?>"/></button>      
-			  <p id="info2"><?=$user2[firstname];?></p> 
-			  <button class="btn btn-info btn-lg btn-block" name="message2" type="submit" value="<?=$user2["Id"];?>" <?=$key_mess2;?>> Сообщение  <?echo $price2?>$</button>
+		 	<div id="block2"> 
+			 	<button id="photo2" type="submit" name="select2" value="<?=$user2["Id"];?>">
+			 		<img  src="<?=$user2["Photo"];?>"width="265" height="265"; alt="<?=$user2["Id"];?>"/>
+				</button> 
+
+			 	 <p id="info2"><?=$user2[firstname];?></p> 
+			  	<button class="btn btn-info btn-lg btn-block" name="message2" type="submit" value="<?=$user2["Id"];?>" <?=$key_mess2;?>> Сообщение  <?echo $price2?>$</button>
 			 
-			  <button class="btn btn-info btn-lg btn-block" name="f2" type="submit" value="<?=$user2["Id"];?>" > добавить в избранное </button>
-		  </div>
-		  
-		  
-	</form>
-	
-	
+			  	<button class="btn btn-info btn-lg btn-block" name="f2" type="submit" value="<?=$user2["Id"];?>" > добавить в избранное </button>
+		  	
+			</div>
+		  		  
+		</form>
+		
+		<a  href="print_mes.php"><input type="button" value="Мои сообщения" name=buttonmess onClick="print_mes.php"></a>
 
-	
-	<a  href="print_mes.php"><input type="button" value="Мои сообщения" name=buttonmess onClick="print_mes.php"></a>
-
-	  <h3>Кредиты: <?echo $_SESSION["credits"];?>$</h3>
+	  	<h3>Кредиты: <?echo $_SESSION["credits"];?>$</h3>
 	  
 	</div>
 	
 	
 	
-	 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </body>
