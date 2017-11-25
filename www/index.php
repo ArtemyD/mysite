11994cd754923315027b989205950a35a96a1cd5
ArtemyD <cocacolamp421@gmail.com>
2017-11-25 15:43:12 +0700

    ошибки

--- www/index.php
+++ www/index.php
@@ -135,8 +135,6 @@
 		$array = json_decode($_COOKIE['user2'], true); 
 			update_likes($_SESSION["select2"], $_SESSION["select1"]);
 
-			update_likes($user2["Id"],$user1["stats"],$user2["stats"], $user1["Id"]);
-
 			header('Location:/');
 	}
 	if(isset($_POST["select3"]))
